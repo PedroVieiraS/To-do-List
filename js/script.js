c1c4b14bfdf26bda2a6e73bf4d6cef573e023248
PedroVieiraS <pedrocorrea978@gmail.com>
2024-08-30 22:23:20 -0300

    teste1

--- js/script.js
+++ js/script.js
@@ -1,9 +1,22 @@
-console.log("Application is running");
-
-
 const btn = document.querySelector(".btn-cadastro")
 const input = document.querySelector(".input-cadastro")
 const ul = document.querySelector(".task-list")
+
+const KEY = "@vieira.key"
+
+const array = [{description: "Vieira", isComplete: true}]
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    localStorage.setItem(KEY, JSON.stringify(array))
+
+    const item = localStorage.getItem(KEY)
+
+    if(item){
+        itemconsumido = JSON.parse(item)
+        console.log(itemconsumido)
+    }
+})
 
 
 btn.addEventListener("click", (event) => {
